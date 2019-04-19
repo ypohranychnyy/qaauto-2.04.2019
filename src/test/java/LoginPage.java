e4e0bd0ee503824e2a09e9587c19014fa1e5fdbe
Yuriy Pohranychnyy <the.whu@gmail.com>
2019-04-19 13:01:55 +0300

    Lesson 3 LinkedIn Successful Login testing and Assert

---
+++ src/test/java/LoginPage.java
@@ -0,0 +1,30 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+
+public class LoginPage {
+    private WebDriver driver;
+    private WebElement loginField;
+    private WebElement passField;
+
+    public LoginPage(WebDriver driver) {
+        this.driver = driver;
+        initElements();
+    }
+
+    public void initElements() {
+        loginField = driver.findElement(By.xpath("//input[@id='login-email']"));
+        passField = driver.findElement(By.xpath("//input[@id='login-password']"));
+
+    }
+
+    public void login(String userEmail, String userPassword) {
+        loginField.sendKeys(userEmail);
+        passField.sendKeys(userPassword);
+        passField.sendKeys(Keys.ENTER);
+
+
+    }
+
+}
--- src/test/java/LoginTest.java
+++ src/test/java/LoginTest.java
@@ -1,91 +1,65 @@
 import org.openqa.selenium.By;
-import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.Assert;
-import org.testng.annotations.Ignore;
 import org.testng.annotations.Test;
-import org.testng.annotations.TestInstance;
-
-import static java.lang.Thread.sleep;
 
 public class LoginTest {
     @Test
-    public void negativeLoginTest(){
+    public void negativeLoginTest() {
         System.setProperty("webdriver.chrome.driver", "D:\\Downloads\\chromedriver_win32\\chromedriver.exe"); //working station
 //         System.setProperty("webdriver.chrome.driver", "/Users/yuriy/Documents/Webdriver/chromedriver"); //for MacOS
         WebDriver driver = new ChromeDriver();
         driver.get("https://www.linkedin.com");
 
-        WebElement loginField = driver.findElement(By.xpath("//input[@id='login-email']"));
-        loginField.sendKeys("t@ukr.net");
-        WebElement passField = driver.findElement(By.xpath("//input[@id='login-password']"));
-        passField.sendKeys("Kosmetista1990");
-        passField.sendKeys(Keys.ENTER);
-
+        LoginPage loginPage = new LoginPage(driver);
+        loginPage.login("t@ukr.net", "Kosmetista1990");
         WebElement errorrorMessage = driver.findElement(By.xpath("//*[@id='error-for-password']"));
-
-        Assert.assertEquals(errorrorMessage.getText(),"Hmm, that's not the right password. Please try again or request a new one.\n");
-
+        Assert.assertEquals(errorrorMessage.getText(), "Hmm, that's not the right password. Please try again or request a new one.\n");
+//        driver.quit();
     }
 
-
     @Test
-    public void negativeLoginTest2(){
+    public void negativeLoginTest2() {
         System.setProperty("webdriver.chrome.driver", "D:\\Downloads\\chromedriver_win32\\chromedriver.exe"); //working station
 //         System.setProperty("webdriver.chrome.driver", "/Users/yuriy/Documents/Webdriver/chromedriver"); //for MacOS
         WebDriver driver = new ChromeDriver();
         driver.get("https://www.linkedin.com");
 
-        WebElement loginField = driver.findElement(By.xpath("//input[@id='login-email']"));
-        loginField.sendKeys("k@ukr.net");
-        WebElement passField = driver.findElement(By.xpath("//input[@id='login-password']"));
-        passField.sendKeys("12341");
-        passField.sendKeys(Keys.ENTER);
-
+        LoginPage loginPage = new LoginPage(driver);
+        loginPage.login("k@ukr.net", "12341");
         WebElement errorrorMessage = driver.findElement(By.xpath("//*[@id='error-for-username']"));
- //       Assert.assertEquals(ErrorrorMessage.getText(),"Please enter a valid email address.");
-        Assert.assertEquals(errorrorMessage.getText(),"Hmm, we don't recognize that email. Please try again.");
+        //       Assert.assertEquals(ErrorrorMessage.getText(),"Please enter a valid email address.");
+        Assert.assertEquals(errorrorMessage.getText(), "Hmm, we don't recognize that email. Please try again.");
+        //        driver.quit();
     }
 
     @Test
     public void SuccessfulLoginTest() {
-
         System.setProperty("webdriver.chrome.driver", "D:\\Downloads\\chromedriver_win32\\chromedriver.exe"); //working station
 //         System.setProperty("webdriver.chrome.driver", "/Users/yuriy/Documents/Webdriver/chromedriver"); //for MacOS
         WebDriver driver = new ChromeDriver();
         driver.get("https://www.linkedin.com");
-
         String loginPageTitle = "LinkedIn: Log In or Sign Up ";
-
         Assert.assertEquals(driver.getTitle(), loginPageTitle);
         System.out.println(driver.getTitle());
 
-        WebElement loginField = driver.findElement(By.xpath("//input[@id='login-email']"));
-        loginField.sendKeys("xodylj@ukr.net");
-        WebElement passField = driver.findElement(By.xpath("//input[@id='login-password']"));
-        passField.sendKeys("Kosmetista1990");
-        passField.sendKeys(Keys.ENTER);
-
-        /*try {
+        LoginPage loginPage = new LoginPage(driver);
+        loginPage.login("xodylj@ukr.net", "Kosmetista1990");
+        /*
+        try {
             sleep(1000);
         } catch (InterruptedException e) {
             e.printStackTrace();
-        }*/
+        }
+        */
 
-        WebElement profileMenuItem = driver.findElement(By.xpath("//li[@id='profile-nav-item']"));
-        Assert.assertTrue(profileMenuItem.isDisplayed(), "Element is not displayed on homepage " + profileMenuItem);
-        Assert.assertEquals(profileMenuItem.getText(), "Профиль");
-        profileMenuItem.click();
-
-//        WebElement profileMenu = driver.findElement(By.xpath("//ul[@id='nav-settings__dropdown-options']/li[2]/h5"));
-        WebElement profileUserName = driver.findElement(By.xpath("//ul[@id='nav-settings__dropdown-options']//h3"));
-        Assert.assertEquals(profileUserName.getText(), "Member1 Number");
-//        Assert.assertEquals(profileMenu.getText(), "УЧЁТНАЯ ЗАПИСЬ");
-        System.out.println(profileUserName.getText());
-
+        HomePage homePage = new HomePage(driver);
+        Assert.assertTrue(homePage.isProfileMenuItemDisplayed());
+        homePage.clickOnProfileMenuItem();
+        Assert.assertEquals(homePage.getProfileUserNameText(), "Member1 Number",
+                "Wromg profile username is displayed");
 //        driver.quit();
-
     }
 }
